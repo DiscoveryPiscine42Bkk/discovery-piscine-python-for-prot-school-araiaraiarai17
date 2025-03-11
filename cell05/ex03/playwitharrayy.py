883c8f823f339a11d26e7f2be9bd39139d5e343c
araiaraiarai17 <40327@prot.ac.th>
2025-03-11 22:16:40 +0700

    Add files via upload

---
+++ cell05/ex03/playwitharrayy.py
@@ -0,0 +1,12 @@
+def process_array(arr):
+    my_arrayy = [my_array for my_array in arr if my_array > 5]
+    myarray = [my_array + 2 for my_array in my_arrayy]
+    myarrayy = set(myarray)
+    return myarrayy
+    
+
+my_array = [2, 8, 9, 48, 8, 22, -12, 2]
+
+print(my_array)
+
+print(process_array(my_array))
