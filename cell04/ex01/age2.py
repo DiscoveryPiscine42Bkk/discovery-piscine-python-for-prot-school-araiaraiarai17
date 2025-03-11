e75e28f7b4f399d065356ba279dc57c49de94051
araiaraiarai17 <40327@prot.ac.th>
2025-03-11 13:02:52 +0700

    Add files via upload

---
+++ cell04/ex01/age2.py
@@ -0,0 +1,11 @@
+age = int(input("Please tell me your age: "))
+agee = input("You are currently 16 years old")
+current_age = 16
+ageee =  current_age + 10
+print("In 10 years, you'll be ",ageee," years old.")
+
+ageeee =  current_age + 20
+print("In 10 years, you'll be ",ageeee," years old.")
+
+ageeeee =  current_age + 30
+print("In 10 years, you'll be ",ageeeee," years old.")
