a1bae5c77e9147d6242df1af636c6975a059c5c4
araiaraiarai17 <40327@prot.ac.th>
2025-03-11 11:52:15 +0700

    Add files via upload

---
+++ cell03/ex03/advanced_mult.py
@@ -0,0 +1,5 @@
+table = 0
+
+for i in range (11) :
+    print("Table de",table,": " , table*0," ",table*1," ",table*2," ",table*3," ",table*4," ",table*5," ",table*6," ",table*7," ",table*8," ",table*9," ",table*10)
+    table += 1
