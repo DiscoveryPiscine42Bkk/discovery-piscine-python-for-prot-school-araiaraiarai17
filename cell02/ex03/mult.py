e25dad88a20192510dbb6e7795364a8403a959d1
araiaraiarai17 <40327@prot.ac.th>
2025-03-11 09:19:48 +0700

    Add files via upload

---
+++ cell02/ex03/mult.py
@@ -0,0 +1,8 @@
+number = int(input("Enter a number: "))
+num = int(input("Enter a number: "))
+if number * num > 0 :
+    print("The result is positive")
+elif number * num < 0 :
+    print("The result is negative")
+elif number * num == 0 :
+    print("The result is positive and negative")
