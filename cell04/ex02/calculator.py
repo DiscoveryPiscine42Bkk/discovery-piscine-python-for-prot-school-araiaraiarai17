2011d49829493139fbe3186ca0fd215aedb9c591
araiaraiarai17 <40327@prot.ac.th>
2025-03-11 13:52:05 +0700

    Add files via upload

---
+++ cell04/ex02/calculator.py
@@ -0,0 +1,15 @@
+num = int(input("Give me the first number : "))
+num2 = int(input("Give me the second number : "))
+tk = input("Thank you")
+
+if num + num2 :
+    print(num, "+" ,num2 ,"=", num + num2)
+    
+if num + num2 :
+    print(num, "-" ,num2 ,"=", num - num2)
+    
+if num + num2 :
+    print(num, "/" ,num2 ,"=", num / num2)
+    
+if num + num2 :
+    print(num, "*" ,num2 ,"=", num * num2)
