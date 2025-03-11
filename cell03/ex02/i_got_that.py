8196a96f639d585632861ca535897734b81eff23
araiaraiarai17 <40327@prot.ac.th>
2025-03-11 11:02:57 +0700

    Add files via upload

---
+++ cell03/ex02/i_got_that.py
@@ -0,0 +1,12 @@
+what = input("What you gotta say? : ")
+while True :
+   
+    whatwhat = input("I got that! Anything else? : ")
+    if what == () :
+        print("I got that! Anything else? : ", what)
+        break
+    elif whatwhat == () :
+        print("I got that! Anything else? : ", whatwhat)
+    elif whatwhat.strip().lower() == "stop..." :
+        print("I got that! Anything else? : STOP")
+        break
